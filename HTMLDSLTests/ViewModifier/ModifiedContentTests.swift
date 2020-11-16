d3adbd4a52f05ecc6630fd841710fcc025c416a4
Shahrukh Alam <alam.shahrukh786@gmail.com>
2020-11-16 17:50:49 +0530

    corrects few Tests

--- HTMLDSLTests/ViewModifier/ModifiedContentTests.swift
+++ HTMLDSLTests/ViewModifier/ModifiedContentTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import HTMLDSL
 
 class ModifiedContentTests: XCTestCase {
-    func testImageWithBackgroundColor() {
+    func testModifiedContentWithSingleBackgroundColor() {
         let image = Image("Images/nora.jpg", alternateText: "Nora Fatehi")
             .backgroundColor(.red)
 
@@ -18,11 +18,10 @@ class ModifiedContentTests: XCTestCase {
         <img src="Images/nora.jpg" alt="Nora Fatehi" style="background-color:red;">
         """
 
-        XCTAssertTrue(image is ModifiedContent<Image, BackgroundColorModifier<Image>>)
         XCTAssertEqual(image.element, expectation)
     }
 
-    func testImageWithMultipleBackgroundColor() {
+    func testModifiedContent_Types_Attributes_Elements() {
         let image = Image("Images/nora.jpg", alternateText: "Nora Fatehi")
         let redImage = image.backgroundColor(.red)
         let greenImage = redImage.backgroundColor(.green)
@@ -45,5 +44,19 @@ class ModifiedContentTests: XCTestCase {
 
         XCTAssertEqual(redImage.element, redExpectation)
         XCTAssertEqual(greenImage.element, greenExpectation)
+    }
+
+    func testModifiedContentWithMultipleBackgroundColor() {
+        let image = Image("Images/nora.jpg", alternateText: "Nora Fatehi")
+            .backgroundColor(.red)
+            .backgroundColor(.green)
+            .backgroundColor(.blue)
+
+        let expectation =
+        """
+        <img src="Images/nora.jpg" alt="Nora Fatehi" style="background-color:blue;">
+        """
+
+        XCTAssertEqual(image.element, expectation)
     }
 }
