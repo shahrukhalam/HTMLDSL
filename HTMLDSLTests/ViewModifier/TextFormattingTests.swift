b477a26f23ef2813b1877bc17f852f0c5a26283b
Shahrukh Alam <alam.shahrukh786@gmail.com>
2020-11-17 01:28:34 +0530

    Adds Text Formatting Support

---
+++ HTMLDSLTests/ViewModifier/TextFormattingTests.swift
@@ -0,0 +1,33 @@
+//
+//  TextFormattingTests.swift
+//  HTMLDSLTests
+//
+//  Created by Shahrukh Alam on 17/11/20.
+//
+
+import XCTest
+@testable import HTMLDSL
+
+class TextFormattingTests: XCTestCase {
+    func testBold() {
+        let paragraphs = Paragraphs("Nora Fatehi".bold)
+
+        let expectation =
+        """
+        <p> <b>Nora Fatehi</b> </p>
+        """
+
+        XCTAssertEqual(paragraphs.element, expectation)
+    }
+
+    func testItalic() {
+        let paragraphs = Paragraphs("Nora " + "Fatehi".italic)
+
+        let expectation =
+        """
+        <p> Nora <i>Fatehi</i> </p>
+        """
+
+        XCTAssertEqual(paragraphs.element, expectation)
+    }
+}
