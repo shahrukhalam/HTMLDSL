fea5933eb9010e3ec2e30346a4b1785c155f91cb
Shahrukh Alam <alam.shahrukh786@gmail.com>
2020-12-24 04:05:16 +0530

    uses ModifiedContent<Self, Modifier> in place of some HTMLView

--- HTMLDSL/ViewModifier/Body + Modifier.swift
+++ HTMLDSL/ViewModifier/Body + Modifier.swift
@@ -8,13 +8,4 @@
 import Foundation
 
 extension HTMLBodyView {
-    func backgroundColor(_ color: Color) -> some HTMLBodyView {
-        let modifier = BackgroundColorModifier<Self>(color: color)
-        return ModifiedContent(content: self, modifier: modifier)
-    }
-
-    func size(width: Dimension? = nil, height: Dimension? = nil) -> some HTMLBodyView {
-        let modifier = SizeModifier<Self>(width: width, height: height)
-        return ModifiedContent(content: self, modifier: modifier)
-    }
 }
