887719473a82a45d926288d155e907dc995b27ed
Shahrukh Alam <alam.shahrukh786@gmail.com>
2020-12-26 05:17:15 +0530

    Adds IndexBody Style

---
+++ HTMLDSL/CSSStyle/IndexBodyStyle.swift
@@ -0,0 +1,27 @@
+//
+//  IndexBodyStyle.swift
+//  HTMLDSL
+//
+//  Created by Shahrukh Alam on 26/12/20.
+//
+
+import Foundation
+
+struct IndexBodyStyle: CSSStyle {
+    let key: CustomStringConvertible = ""
+    let styles = [Style]()
+    
+    var element: String
+    
+    init() {
+        let bodyStyle = TagStyle(for: .enclosing(.body))
+            .margin(uniform: .pixel(16))
+            .font(family: .sansSerif([.SFProDisplay,
+                                      .SFProIcons,
+                                      .HelveticaNeue,
+                                      .Helvetica,
+                                      .Arial]))
+        
+        self.element = bodyStyle.element
+    }
+}
