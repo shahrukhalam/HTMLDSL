97de204f0e12023adbd2fd54256ee0800e517cb2
Shahrukh Alam <alam.shahrukh786@gmail.com>
2022-08-09 14:03:15 +0530

    adds one more block

--- Sources/HTMLDSL/View/ViewBuilder.swift
+++ Sources/HTMLDSL/View/ViewBuilder.swift
@@ -37,4 +37,9 @@ public struct ViewBuilder {
         let views = [AnyView(c0), AnyView(c1), AnyView(c2), AnyView(c3), AnyView(c4), AnyView(c5)]
         return AnyView(views)
     }
+    
+    public static func buildBlock<C0, C1, C2, C3, C4, C5, C6>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6) -> AnyView where C0 : HTMLContentView, C1 : HTMLContentView, C2 : HTMLContentView, C3 : HTMLContentView, C4 : HTMLContentView, C5 : HTMLContentView, C6 : HTMLContentView {
+        let views = [AnyView(c0), AnyView(c1), AnyView(c2), AnyView(c3), AnyView(c4), AnyView(c5), AnyView(c6)]
+        return AnyView(views)
+    }
 }
