c84a8e16698e05815210d9ccf0bec15a7d4acb04
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-08-19 19:34:05 +0530

    Adds VideoEmbed

--- Sources/HTMLDSL/Body/Embed.swift
+++ Sources/HTMLDSL/Body/Embed.swift
@@ -6,6 +6,19 @@ public struct Embed: HTMLBodyContentView {
     public init(_ source: String) {
         self.attributes += [
             .source(source),
+            .key("frameborder", value: "0")
+        ]
+    }
+}
+
+public struct VideoEmbed: HTMLBodyContentView {
+    public let body: String = .empty
+    public let tag = Tag.enclosing(.iframe)
+    public var attributes = [Attribute]()
+
+    public init(_ source: String) {
+        self.attributes += [
+            .source(source),
             .key("allow", value: "autoplay; fullscreen; encrypted-media; picture-in-picture"),
             .value("allowfullscreen"),
             .key("frameborder", value: "0")
